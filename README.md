dc716a172b9a84d8bcb234f61a84744ceaa48625
Erik <esnetto@gmail.com>
2022-07-13 11:58:31 -0300

    Upload API code

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # nft-bridge-api
 API for NFT bridge
+
+# Usage
+This API will read properties of a CallistoNFT standard NFT with Classified extension.
+
+## Supported chain IDs:
+Callisto Mainnet: 820
+Callisto Testnet: 20729
+Binance Smart Chain: 56
+Polygon: 137
+
+[API URL]/.netlify/functions/server/:chainID/:contractAddress/:tokenID
---
+++ express/server.js
@@ -0,0 +1,46 @@
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Web3 = require('web3-eth');
+
+const router = express.Router();
+
+const providerRPC = {
+    '820': 'https://rpc.callisto.network/',
+    '20729': 'https://testnet-rpc.callisto.network',
+    '56' : 'https://bsc-dataseed.binance.org/',
+    '137' : 'https://polygon-rpc.com/'
+};
+
+//Contract
+const getClassForTokenIDABI = {"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"getClassForTokenID","inputs":[{"type":"uint256","name":"_tokenID","internalType":"uint256"}]}
+const getClassPropertiesABI = {"type":"function","stateMutability":"view","outputs":[{"type":"string[]","name":"","internalType":"string[]"}],"name":"getClassProperties","inputs":[{"type":"uint256","name":"_classID","internalType":"uint256"}]}
+
+async function getClassProperties(chainID, contractAddress, tokenID) {
+    let web3 = new Web3(providerRPC[chainID]);
+    let contract = new web3.Contract([getClassForTokenIDABI, getClassPropertiesABI], contractAddress)
+    let classID = await contract.methods.getClassForTokenID(tokenID).call()
+    return JSON.parse("["+await contract.methods.getClassProperties(classID).call()+"]")
+}
+
+router.get('/:chainID/:contractAddress/:tokenID', async (req, res) => {
+    let json = await getClassProperties(req.params.chainID, req.params.contractAddress, req.params.tokenID)
+    res.setHeader('Content-Type', 'application/json');
+    res.send(json)
+});
+
+app.use(
+    cors({
+        //credentials: true,
+        origin: '*'
+    })
+);
+app.options('*', cors());
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+module.exports = app;
+module.exports.handler = serverless(app);
